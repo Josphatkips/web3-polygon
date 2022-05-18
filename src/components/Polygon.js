3200e5ebbcdce74885e809d95d7bc9724bc462e9
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-18 15:48:31 +0300

    initial commit

---
+++ src/components/Polygon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Polygon = () => {
+    // const web3 = new Web3('https://rpc-mumbai.matic.today'); //test
+
+// 0x6DC772CF5f51916aAB8C6D6d3BD0A9ccE25Ee519
+// 0x4e071db91d22497880417e0922a513be817e3e5512ba22e9e9e9f1d53ae8e201
+// https://mumbaifaucet.com/ 
+
+const myaddress= "0x6DC772CF5f51916aAB8C6D6d3BD0A9ccE25Ee519"
+const mykey= "0x4e071db91d22497880417e0922a513be817e3e5512ba22e9e9e9f1d53ae8e201"
+
+const tokenaddress= "0x449897c439A87af2553784839BA2Fc5E95693992"
+const privateKey1Buffer = Buffer.from(mykey, 'hex')
+    return ( <>
+    </> );
+}
+ 
+export default Polygon;
