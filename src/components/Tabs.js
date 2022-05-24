5f057105ce1133818c72fee6a5dfc5ba7a1784b1
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-24 22:35:12 +0300

    done with Address creation and balance check

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import { Tab } from '@headlessui/react'
 import Stripe from './Stripe'
+import { ethers } from "ethers";
+const provider = new ethers.providers.JsonRpcProvider('https://speedy-nodes-nyc.moralis.io/5b4db9130fec05462817ab17/polygon/mumbai');
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function Tabs() {
+  const [newwallet, setNewWallet]=useState({})
+  const [balance, setBlance]=useState('')
+
+  async function generateWallet(){
+    const  wallet =  ethers.Wallet.createRandom()
+    // const  wallet =  new ethers.Wallet( mykey,  provider  )
+    setNewWallet(wallet)
+    console.log(wallet.getBalance())
+
+    // const bal = await wallet.getBalance();
+
+    const balance = await provider.getBalance(wallet.address);
+    console.log(balance.toString()); // 0
+    setBlance(balance.toString())
+}
+
   let [categories] = useState({
     "Buy Asset": [
       {
@@ -40,6 +58,13 @@ export default function Tabs() {
     ],
    
   })
+
+  async function getMyBlance(){
+    const balance = await provider.getBalance(newwallet.address);
+    console.log(balance.toString()); 
+
+    return balance.toString()
+  }
 
   return (
     <div className="w-full max-w-md px-2 py-16 sm:px-0">
@@ -83,21 +108,42 @@ export default function Tabs() {
               )}
             >
                 {/* Wallet */}
+                Your Balance is: {balance}
+                <div class="mb-4">
+              <label class="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
+                Address
+              </label>
+              <textarea
+                class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                id="emailaddress"
+                type="text"
+                value={newwallet.address}
+                readOnly
+                // placeholder="you@somewhere.com"
+              />
+            </div>
+                <div class="mb-4">
+              <label class="block text-blue-300 py-2 font-bold mb-2" for="emailaddress">
+                Your Private Key
+              </label>
+              <textarea
+                class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                id="emailaddress"
+                type="text"
+                value={newwallet.privateKey}
+                readOnly
+                // placeholder="you@somewhere.com"
+              />
+            </div>
 
                 <div className='flex flex-col'>
-                    <div>
-                        <span className='text-indigo-400 text-2xl font-black'>My Wallet</span>
-                        <div class="flex items-center justify-between pt-4">
-                        
-                        </div>
-                        
-                    </div>
+                    
                     <div>
                     <div class="flex items-center justify-between pt-4">
                         <button
                             class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
                             type="button"
-                            onClick={()=>alert()}
+                            onClick={()=>generateWallet()}
                         >
                             Create Wallet
                         </button>
