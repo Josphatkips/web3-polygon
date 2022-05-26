c22bc03db97a6738cb1d5697e5c856101c2b4159
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-26 23:55:42 +0300

    done with authentication pages UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Polygon from './components/Polygon';
 import CreateWallet from './components/CreateWallet';
+import { Routes, Route, Link } from "react-router-dom";
+import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       
       {/* <Polygon /> */}
-      <CreateWallet />
+      {/* <CreateWallet /> */}
+      {/* <Nav /> */}
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/create-account" element={<SignUp />} />
+        <Route path="/dashboard" element={<CreateWallet />} />
+      </Routes>
     </div>
   );
 }
