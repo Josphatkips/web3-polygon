499751e4aaffbd012d31cb1da3e1b14f79e38b44
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-27 14:46:08 +0300

    done with registration

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -16,31 +16,54 @@
 */
 import { LockClosedIcon } from '@heroicons/react/solid'
 import axios from 'axios'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { Link,Navigate } from 'react-router-dom'
+import { AuthContext } from '../../App'
 import url from '../url'
 
 export default function SignUp() {
 
     const [email, setEmail]= useState('')
     const [name, setName]= useState('')
+    const [spin, setSpin]= useState(false)
+    const [success, setSucess]= useState(false)
     const [password, setPassword]= useState('')
     const [confirm_password, setConfirmPassword]= useState('')
     const [error, setError]=useState('')
     const [errors, setErrors]=useState({})
-    const [showAlert, setShowAlert] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const { auth, dispatchAuth } = useContext(AuthContext)
 
     const createAccount=()=>{
 
       setError('')
       setErrors({})
       setShowAlert(false)
+      setSpin(true)
 
       axios.post(url+'auth/register',{
         confirm_password, password,email,name
 
       }).then(res=>{
         console.log(res)
+
+        if (res.data.success) {
+          // alert('success')
+          // 1. Dispatch to context 
+          var abc= res.data.result
+          dispatchAuth({type:'login_status',value:true})
+          dispatchAuth({type:'user',value:JSON.stringify(abc)})
+          // 2. write to session storage
+
+          sessionStorage.setItem('logged_in', true);
+          sessionStorage.setItem('user', JSON.stringify(abc));
+          sessionStorage.setItem('token', res.data.result.accessToken);
+
+          setSucess(true)
+        } else {
+          // setError('Wrong username or password')
+        }
 
       }).catch(e=>{
         
@@ -56,6 +79,7 @@ export default function SignUp() {
           setShowAlert(true)
         }
       }).then(res=>{
+        setSpin(false)
         
 
       })
@@ -93,6 +117,12 @@ export default function SignUp() {
     )
   return (
     <>
+
+    {auth.logged_in?<>
+    <Navigate to="/dashboard" />
+    
+    
+    </>:null}
       {/*
         This example requires updating your template:
 
@@ -125,7 +155,10 @@ export default function SignUp() {
         {rendererror()}
         </>
       ) : null}
+
+
           </div>
+          
           <div className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm  space-y-2">
@@ -191,7 +224,7 @@ export default function SignUp() {
                   <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
                 {errors.password}
               </span>
-                </>:<></>}
+                </>:null}
                 
               </div>
               <div>
@@ -237,6 +270,17 @@ export default function SignUp() {
               </div>
             </div>
 
+            {spin?<>
+
+              <div className='items-center mx-auto w-full'>
+          <svg role="status" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-indigo-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          </div>
+            
+            </>:null}
+            
             <div>
               <button
                 type="submit"
