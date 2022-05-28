a71195c90ff70ac824c10c31f8c62fb6383f8b89
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-28 17:04:46 +0300

    done with MVP

--- src/components/ConfirmPay.js
+++ src/components/ConfirmPay.js
@@ -18,6 +18,8 @@ const Confirm = () => {
     const [amount, setAmount]=useState('')
     const [error, setError]=useState('')
 
+    const [spin, setSpin]=useState(false)
+
     async function fetchBal(){
 
         const conv= ethers.utils.formatEther(amount)
@@ -40,6 +42,13 @@ const Confirm = () => {
 
 
     const fetchCred=()=>{
+
+      if(address=='' || amount==''){
+        alert('Fill in the form')
+
+        return
+      }
+      setSpin(true)
         var token = '3|uZERsMsjWM7LJb2fql0YxA99msdMF9efxvINo6vt'
   
         const config = {
@@ -112,6 +121,7 @@ const Confirm = () => {
         contract.transfer(to_address, numberOfTokens).then((transferResult) => {
           console.log(transferResult)
           alert("sent token")
+          setSpin(false)
         })
       } // ether send
       else {
@@ -130,6 +140,7 @@ const Confirm = () => {
         try {
           walletSigner.sendTransaction(tx).then((transaction) => {
             console.dir(transaction)
+            
             alert("Send finished!")
           })
         } catch (error) {
@@ -143,18 +154,29 @@ const Confirm = () => {
         <div className='flex md:w-1/2 flex-col mx-auto' >
             <div class="mb-6">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Address to Send to</label>
-            <input type="email" id="email" value={address} onChange={(e)=>setAddress(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="name@flowbite.com" required="" />
+            <input type="email" id="email" value={address} onChange={(e)=>setAddress(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="Address" required="" />
             </div>
             <div class="mb-6">
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Amount</label>
-            <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required="" />
+            <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder='number of tokens' required="" />
             </div>
             <div class="flex items-start mb-6">
+            {spin?<>
+
+<div className='items-center mx-auto w-full'>
+<svg role="status" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-indigo-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+</svg>
+</div>
+
+</>:null}
             {/* <div class="flex items-center h-5">
             <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
             </div> */}
             {/* <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label> */}
             </div>
+
             <button 
             onClick={()=>fetchCred()}
              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
