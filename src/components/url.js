cce8a3e4be4ed14487c393e4960efac48c59e163
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-28 15:29:40 +0300

    updated the Url

--- src/components/url.js
+++ src/components/url.js
@@ -1,3 +1,4 @@
-const url = "http://localhost:8000/api/" //<-- test api
+// const url = "http://localhost:8000/api/" //<-- test api
+const url = "https://rv-admin.roycehub.com/api/" //<-- test api
 
 export default url
